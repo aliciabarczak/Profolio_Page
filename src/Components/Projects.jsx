f70aa2dce4d4463d75c18533ddecd50a6face9f1
Alicia Barczak <aliciabarczak@Alicias-MacBook-Air.local>
2022-06-23 13:36:33 +0100

    add basic nav bar to the Projects component

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,1 +1,12 @@
-export default function () {}
+import "./../Styling/Projects.css";
+export default function () {
+  return (
+    <>
+      <div id="miniNav">
+        <div>Balance Book</div>
+        <div>NC News</div>
+        <div>React App</div>
+      </div>
+    </>
+  );
+}
