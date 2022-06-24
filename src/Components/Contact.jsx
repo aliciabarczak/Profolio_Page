82f2e5879b021f9715aae265a87b7deafd9b98a2
Alicia Barczak <aliciabarczak@Alicias-MacBook-Air.local>
2022-06-24 20:33:50 +0100

    add heading to Contact page

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,6 +1,11 @@
 import "./../Styling/Contact.css"
 export default function Contact() {
     return (<div id="ContactPage">
+         <header>
+        <h1 class="decorated">
+          <span>Get in touch</span>
+        </h1>
+      </header>
         <div className="contactBox">
         <img src="https://i.ibb.co/4KM38Bk/linkedin-3-128.png"/>
         <p>Alicia Barczak</p>
