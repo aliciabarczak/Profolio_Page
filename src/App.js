0114966b967bcf808d39911b8f8c0d71f409d845
Alicia Barczak <aliciabarczak@Alicias-MacBook-Air.local>
2022-06-24 19:32:44 +0100

    add basic Blogs component

--- src/App.js
+++ src/App.js
@@ -4,21 +4,23 @@ import AboutMePage from "./Components/AboutMePage";
 import Homepage from "./Components/Homepage";
 import Nav from "./Components/Nav";
 import Projects from "./Components/Projects";
+import Blogs from "./Components/Blogs"
 
 function App() {
   return (
     <div className="App">
       {/* <Nav /> */}
       <main>
-        <section id="home">
+        <section id="homepage">
           <Homepage />
         </section>
         <section id="about">
           <AboutMePage />
         </section>
-        <section>
+        <section id="experience">
           <Projects />
         </section>
+     <section><Blogs /></section>
       </main>
 
       {/* <Footer /> */}
---
+++ src/Components/Blogs.jsx
@@ -0,0 +1,6 @@
+import "./../Styling/Blogs.css"
+export default function Contact(){
+    return (<header>
+        <h2 className="decorated" id="BlogsHeader"><span>Blogs</span></h2>
+        </header>)
+}
