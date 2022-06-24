39f651d5d06ec5648ba9fee6a9ac2e719655cb48
Alicia Barczak <aliciabarczak@Alicias-MacBook-Air.local>
2022-06-24 20:29:27 +0100

    add contact details to Contact page

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,3 +1,18 @@
+import "./../Styling/Contact.css"
 export default function Contact() {
-    return 
+    return (<div id="ContactPage">
+        <div className="contactBox">
+        <img src="https://i.ibb.co/4KM38Bk/linkedin-3-128.png"/>
+        <p>Alicia Barczak</p>
+        </div>
+        <div className="contactBox">
+        <img src="https://i.ibb.co/XJq5vb6/github-11-128.png"/>
+        <p>aliciabarczak</p>
+        </div>
+        <div className="contactBox">
+        <img src="https://i.ibb.co/z4G8Bcq/email-14-128.png"/>
+        <p>aliciabarczak1@gmail.com</p>
+        </div>
+    </div>)
 }
+
